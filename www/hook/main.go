b7322ab36530d7d25ba83490e1d603ecc78169b3
gaopengfei <gaopengfei@soyoung.com>
2018-12-12 14:06:49 +0800

    微调

--- www/hook/main.go
+++ www/hook/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"net/http"
+	"os/exec"
 )
 
 func main() {
+	// execCommand()
+	serverStart()
+}
+
+func execCommand() {
+	cmd := exec.Command("/bin/bash", "git_pull.sh")
+	var out bytes.Buffer
+
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s", out.String())
+
+}
+
+func serverStart() {
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
@@ -19,6 +39,14 @@ func main() {
 
 	r.POST("hook", func(c *gin.Context) {
 		var jsonp map[string]interface{}
+
+		var gitStruct GitHubWebHook
+
+		err := c.BindJSON(&gitStruct)
+		if err == nil {
+			resp := gitStruct.Repository.GitURL
+			fmt.Printf("gitUrl: %s \n", resp)
+		}
 
 		if c.BindJSON(&jsonp) == nil {
 			saveToFile(jsonp)
@@ -43,4 +71,40 @@ func saveToFile(jsonp map[string]interface{}) {
 	if err != nil {
 		fmt.Println("error:", err)
 	}
+}
+
+// GitHubWebHook github的json结构
+type GitHubWebHook struct {
+	Zen    string
+	HookID int `json:"hook_id"`
+	Hook   struct {
+		Type   string
+		ID     int
+		Name   string
+		Active bool
+		Events []string
+		Config struct {
+			ContentType string `json:"content_type"`
+			InsecureSSL string `json:"insecure_ssl"`
+			Secret, URL string
+		}
+		UpdatedAt             string `json:"updated_at"`
+		CreatedAt             string `json:"created_at"`
+		URL, TestURL, PingURL string
+		LastResponse          struct {
+			Code    interface{}
+			Status  string
+			Message interface{}
+		}
+	}
+	Repository struct {
+		ID                     int
+		Name, FullName, NodeID string
+		Private                bool
+		Owner                  map[string]interface{}
+		GitURL                 string `json:"git_url"`
+		SSHURL                 string `json:"ssh_url"`
+		CloneURL               string `json:"clone_url"`
+	}
+	Sender map[string]interface{}
 }
